64b50497e7f5a556cd897e6e6af814c5d981ea26
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2024-01-03 01:21:28 +0330

    Index raw completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,4 @@ document.addEventListener('DOMContentLoaded' , function () {
 
 
 
-    // let outputElm = document.querySelector('.md');
-    // let textarea = document.querySelector('textarea');
-    //
-    // textarea.addEventListener('input' , (e) => {
-    //     outputElm.innerHTML = marked.parse(e.target.value);
-    // });
-    //
-    // outputElm.innerHTML = marked.parse(document.querySelector('textarea').value);
-
 })
