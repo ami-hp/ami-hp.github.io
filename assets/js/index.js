838865b49bb277bc71b88a94869adf8804343682
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2024-01-05 00:10:22 +0330

    Added Alpine Intersect & fixed sizing

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,21 @@ document.addEventListener('DOMContentLoaded' , function () {
 
 
 })
+
+// gsap.registerPlugin(ScrollTrigger);
+//
+// const lenis = new Lenis({
+//     duration: 1.2,
+//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+// });
+//
+// lenis.on('scroll', (e) => {
+//     console.log(e)
+// })
+//
+// function raf(time) {
+//     lenis.raf(time)
+//     requestAnimationFrame(raf)
+// }
+//
+// requestAnimationFrame(raf)
