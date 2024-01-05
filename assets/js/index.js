46c0e00792afd29f229f8655080e7e1b1d893ecf
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2024-01-05 19:02:23 +0330

    optimize and delete lenis

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,22 +5,22 @@ import '../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded' , function () {
 
-    gsap.registerPlugin(ScrollTrigger);
-
-    const lenis = new Lenis({
-        duration: 1.2,
-    });
-
-    lenis.on('scroll', (e) => {
-        console.log(e)
-    })
-
-    function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
+    // gsap.registerPlugin(ScrollTrigger);
+    //
+    // const lenis = new Lenis({
+    //     duration: 1.2,
+    // });
+    //
+    // lenis.on('scroll', (e) => {
+    //     console.log(e)
+    // })
+    //
+    // function raf(time) {
+    //     lenis.raf(time)
+    //     requestAnimationFrame(raf)
+    // }
+    //
+    // requestAnimationFrame(raf)
 
 })
 
